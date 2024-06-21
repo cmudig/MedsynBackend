ab5aa1b09ae0c6891bf0dc385e4c20b9df4709bc
TomWartm <tomw@ethz.ch>
2024-06-21 20:50:57 +0000

    stream print statement to clients

--- src/app.py
+++ src/app.py
@@ -1,37 +1,39 @@
-from flask import Flask, request, jsonify, send_from_directory, abort
+from flask import Flask, request, jsonify, send_from_directory, abort, Response, render_template_string
 import os
 from extract_text import TextExtractor
 from stage1 import run_diffusion_1
 from stage2 import run_diffusion_2
-import torch
+import threading
+import time
+import io
+import sys
+from flask_cors import CORS
 
 app = Flask(__name__)
 
+# Enable CORS
+CORS(app, resources={r"/*": {"origins": "*"}})
+
 # Define the folder to serve files from
 FILES_FOLDER = '/media/volume/gen-ai-volume/MedSyn/results'
-
-
 
 # models
 TEXTEXTRACTOR_MODEL_FOLDER = "/media/volume/gen-ai-volume/MedSyn/models/test_run2"
 STAGE1_MODEL_FOLDER="/media/volume/gen-ai-volume/MedSyn/models/stage1"
 STAGE2_MODEL_FOLDER = "/media/volume/gen-ai-volume/MedSyn/models/stage2"
 
+process_is_running= False
 
-# Define a route to listen to GET requests
-@app.route('/api', methods=['GET'])
-def api_get():
-    data = request.args.to_dict()
-    return jsonify(data), 200
+# check if server is running
+@app.route('/')
+def base_route():
+    return jsonify({"server_running": True})
 
 # Define a route to listen to POST requests
 @app.route('/api', methods=['POST'])
 def api_post():
     data = request.json
     return jsonify(data), 200
-
-
-
 
 # lists all files in a folder
 @app.route('/files/<foldername>', methods=['GET'])
@@ -70,7 +72,6 @@ def get_file(foldername, filename):
 @app.route('/files/<filename>', methods=['POST'])
 def process_text(filename):
     try:
-        
         # Get the prompt from the POST request
         data = request.get_json()
         prompt = data.get('prompt')
@@ -84,36 +85,74 @@ def process_text(filename):
         filename = filename+'.npy'
         print(f"filename: {filename}")
 
-        
-        # Run the text extractor
-        text_extractor = TextExtractor(resume_model=TEXTEXTRACTOR_MODEL_FOLDER)
-        print("init worked")
-        text_extractor.run(prompt, output_folder, filename)
-        print(f"Textembedding stored in: {output_folder}")
+        # Start the process in a separate thread
+        threading.Thread(target=run_text_extractor_and_models, args=(prompt, output_folder, filename)).start()
 
-
-        # Run low-res model
-        run_diffusion_1(input_folder=FILES_FOLDER+"/text_embed", 
-                output_folder= FILES_FOLDER +"/img_64_standard", 
-                model_folder=STAGE1_MODEL_FOLDER, 
-                num_sample=1)
-        
-        # Run high-res model
-        run_diffusion_2(input_folder=FILES_FOLDER+ "/img_64_standard", 
-                output_folder=FILES_FOLDER +"/img_256_standard", 
-                model_folder=STAGE1_MODEL_FOLDER) 
-        
-
-        return jsonify({"message": "Process completed successfully",
-                        "filename":filename,
-                        "text_embed_foldername":FILES_FOLDER+"/text_embed",
-                        "low_res_foldername":FILES_FOLDER +"/img_64_standard",
-                        "high_res_foldername":FILES_FOLDER +"/img_256_standard",
-                        "get_request":f'files/{filename[:-4]}',
-                        "prompt":prompt}), 200
+        return jsonify({"message": "Process started", "filename": filename}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+@app.route('/progress')
+def progress():
+    def generate():
+        with open('log.txt') as f:
+            f.seek(0, os.SEEK_END)  # Move the file pointer to the end of the file
+            while True:
+                line = f.readline()
+                if line:
+                    yield f"{line.strip()}\n"
+                #time.sleep(1)  # Sleep briefly to avoid busy-waiting
+    return Response(generate(), content_type='text/event-stream')
+
+@app.route('/running', methods=['POST'])
+def check_running():
+    global process_is_running
+    return jsonify({"process_is_running": process_is_running})
+
+def run_text_extractor_and_models(prompt, output_folder, filename):
+    global process_is_running
+    old_stdout = sys.stdout
+    sys.stdout = StreamToFile()
+    process_is_running = True
+
+    try:
+        # # Run the text extractor
+        # text_extractor = TextExtractor(resume_model=TEXTEXTRACTOR_MODEL_FOLDER)
+        # text_extractor.run(prompt, output_folder, filename)
+        # print(f"Textembedding stored in: {output_folder}")
+
+        # # Run low-res model
+        # run_diffusion_1(input_folder=FILES_FOLDER+"/text_embed", 
+        #                 output_folder=FILES_FOLDER +"/img_64_standard", 
+        #                 model_folder=STAGE1_MODEL_FOLDER, 
+        #                 num_sample=1)
+
+        # # Run high-res model
+        # run_diffusion_2(input_folder=FILES_FOLDER+ "/img_64_standard", 
+        #                 output_folder=FILES_FOLDER +"/img_256_standard", 
+        #                 model_folder=STAGE1_MODEL_FOLDER)
+
+        # fake some progress for now
+        for i in range(100):
+            time.sleep(5)
+            print(f"progress: {i}")
+    finally:
+        sys.stdout = old_stdout
+        process_is_running=False
+
+class StreamToFile(io.StringIO):
+    def __init__(self):
+        super().__init__()
+        self.file = open('log.txt', 'w')
+
+    def write(self, message):
+        self.file.write(message)
+        self.file.flush()
+        super().write(message)
+
+    def close(self):
+        self.file.close()
+        super().close()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
@@ -123,7 +162,6 @@ if __name__ == '__main__':
 """
 # access server through public api
 $ curl "http://149.165.171.65:5000/api?marco=polo"
-
 
 
 """
