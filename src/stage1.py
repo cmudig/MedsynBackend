11be8e27978f0d712288ce4e43f169619bb046e2
Katelyn Morrison <johndoe@example.com>
2025-02-14 02:37:00 +0000

    fixed stage1

--- src/stage1.py
+++ src/stage1.py
@@ -1409,11 +1409,6 @@ class Trainer(object):
 
                                 # Average over time steps
                                 avg_attention_map = time_attention_maps.mean(dim=0)  # [batch_size, frames, query_len, key_len]
-                                # Stack attention maps per frame
-                                attention_maps_per_frame = np.stack(attention_maps_per_frame)  # [frames, H, W]
-
-                                attention_save_path = os.path.join(self.attention_folder, f"{file_name}_token_{token_idx}_{token_str}_heatmaps.npy")
-                                print("ATTENTION PATH: ", attention_save_path)
 
                                 # Map attention maps back to tokens
                                 for token_idx in range(tokens.shape[1]):
@@ -1432,6 +1427,9 @@ class Trainer(object):
                                     # Stack attention maps per frame
                                     attention_maps_per_frame = np.stack(attention_maps_per_frame)  # [frames, H, W]
                                     # Save the attention maps for this token
+                                    attention_save_path = os.path.join(self.attention_folder, f"{file_name}_token_{token_idx}_{token_str}_heatmaps.npy")
+                                    print("ATTENTION PATH: ", attention_save_path)
+
                                     np.save(attention_save_path, attention_maps_per_frame)
                                     print(f"Saved heatmap for token '{token_str}' at: {attention_save_path}")
                             else:
@@ -1475,9 +1473,6 @@ class Trainer(object):
                                 # Average over time steps
                                 avg_attention_map = time_attention_maps.mean(dim=0)  # [batch_size, frames, query_len, key_len]
 
-                                attention_save_path = os.path.join(self.attention_folder, file_name.replace(".npy", "_attention.npy"))
-                                print("ATTENTION PATH: ", attention_save_path)
-
                                 # Map attention maps back to tokens
                                 for token_idx in range(tokens.shape[1]):
                                     token_id = tokens[0, token_idx]
@@ -1495,6 +1490,9 @@ class Trainer(object):
                                     # Stack attention maps per frame
                                     attention_maps_per_frame = np.stack(attention_maps_per_frame)  # [frames, H, W]
                                     # Save the attention maps for this token
+                                    attention_save_path = os.path.join(self.attention_folder, f"{file_name}_token_{token_idx}_{token_str}_heatmaps.npy")
+                                    print("ATTENTION PATH: ", attention_save_path)
+
                                     np.save(attention_save_path, attention_maps_per_frame)
                                     print(f"Saved heatmap for token '{token_str}' at: {attention_save_path}")
                             else:
